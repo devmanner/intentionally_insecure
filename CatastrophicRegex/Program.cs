9d636abd83a51dbd416d62aa6a39697f63c55218
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2024-11-22 17:02:56 +0100

    Added another example

---
+++ CatastrophicRegex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CatastrophicRegex
+{
+    class Program
+    {
+
+        static bool test(string regex, string s)
+        {
+            Console.WriteLine($"Starting testcase");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var r = Regex.Match(s, regex).Success;
+            Console.WriteLine($"S: {s} RE: {regex} match? {r}");
+
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time for testcase with length: {s.Length} is {watch.ElapsedMilliseconds} ms");
+            return r;
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Don't go longer than this...
+            for (var i = 0; i < 25; ++i)
+            {
+                test(@"(a+)+$", new string('a', i) + "X");
+            }
+        }
+    }
+}
